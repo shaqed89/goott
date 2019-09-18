92e1d2237286305464b7f65c97b414aef7c4cf3c
shaqed89 <53854917+shaqed89@users.noreply.github.com>
2019-09-18 14:40:36 +0900

    메인페이지 슬라이드
    작업 중

--- kimbo/WebContent/resource/main_script1.js
+++ kimbo/WebContent/resource/main_script1.js
@@ -1,4 +1,16 @@
 /**
  * 
  */
-$(function(){});
+$(function(){
+	$("#main_slide_img").bxSlider({
+		mode : 'fade',
+		slideWidth : 600,
+		slideHeight : 400,
+		auto : true,
+		speed : 4000,
+		randomStart : true,
+		infiniteloop : true
+	});
+	
+	$("#main_slide_img").removeClass("bx-wrapper");
+});
