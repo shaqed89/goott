e8f706be178e54da0bd1495b7be85e694e7a6aea
shaqed89 <53854917+shaqed89@users.noreply.github.com>
2019-09-30 20:01:27 +0900

    수정 진행 중

--- kimbo/WebContent/resource/kimbo.js
+++ kimbo/WebContent/resource/kimbo.js
@@ -2,6 +2,19 @@
  * 
  */
 $(function(){
+	//배경화면
+	/*    $("body").backstretch([
+    "../img/main_kim/main_slide1.jpg",
+    "../img/main_kim/main_slide2.jpg",
+    "../img/main_kim/main_slide3.jpg",
+    "../img/main_kim/main_slide4.jpg"
+    ], {
+      fade: 1000,
+      duration: 3000
+  	});*/
+	
+	
+	
 	$("#header_menu2").css("top","0px");
 	$("#header_menu1").css("opacity","0");
 	
@@ -14,13 +27,16 @@ $(function(){
 		$("#header_menu2").css("top","0px");
 	});
 	
-    $("body").backstretch([
-        "../img/main_kim/main_slide1.jpg",
-        "../img/main_kim/main_slide2.jpg",
-        "../img/main_kim/main_slide3.jpg",
-        "../img/main_kim/main_slide4.jpg"
-        ], {
-          fade: 1000,
-          duration: 3000
-      });
+	$("#main_page").carousel({interval: false});
+	
+	//top5
+	var top5_list="";
+	var nameArr=["tattooist1","tattooist2","tattooist3","tattooist4","tattooist5",];
+	
+	for(i=0; i<5; i++){
+		top5_list+="<li id='tattooist"+(i+1)+"'>" +
+				"<img src='../img/main_kim/tattooist"+(i+1)+".jpg'/>" +
+				"</li>";
+	}
+	$("#main_top5").html(top5_list);
 });
