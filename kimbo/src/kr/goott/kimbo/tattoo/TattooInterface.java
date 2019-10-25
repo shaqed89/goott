6f886bdc76d161dd8b57bec844dc2691cec1e49c
shaqed89 <53854917+shaqed89@users.noreply.github.com>
2019-10-25 19:08:08 +0900

    커밋

---
+++ kimbo/src/kr/goott/kimbo/tattoo/TattooInterface.java
@@ -0,0 +1,9 @@
+package kr.goott.kimbo.tattoo;
+
+public class TattooInterface {
+	//장르 불러오기
+
+	//부위 불러오기
+	
+	//주제 불러오기
+}
--- kimbo/src/kr/goott/kimbo/tattoo/TattooVO.java
+++ kimbo/src/kr/goott/kimbo/tattoo/TattooVO.java
@@ -22,5 +22,113 @@ public class TattooVO {
 	private String oldFile2;
 	private String oldFile3;
 ///////////////////////////////////
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPart() {
+		return part;
+	}
+	public void setPart(String part) {
+		this.part = part;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getSigan() {
+		return sigan;
+	}
+	public void setSigan(String sigan) {
+		this.sigan = sigan;
+	}
+	public String getTattooFile1() {
+		return tattooFile1;
+	}
+	public void setTattooFile1(String tattooFile1) {
+		this.tattooFile1 = tattooFile1;
+	}
+	public String getTattooFile2() {
+		return tattooFile2;
+	}
+	public void setTattooFile2(String tattooFile2) {
+		this.tattooFile2 = tattooFile2;
+	}
+	public String getTattooFile3() {
+		return tattooFile3;
+	}
+	public void setTattooFile3(String tattooFile3) {
+		this.tattooFile3 = tattooFile3;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getOldFile1() {
+		return oldFile1;
+	}
+	public void setOldFile1(String oldFile1) {
+		this.oldFile1 = oldFile1;
+	}
+	public String getOldFile2() {
+		return oldFile2;
+	}
+	public void setOldFile2(String oldFile2) {
+		this.oldFile2 = oldFile2;
+	}
+	public String getOldFile3() {
+		return oldFile3;
+	}
+	public void setOldFile3(String oldFile3) {
+		this.oldFile3 = oldFile3;
+	}
 	
 }
